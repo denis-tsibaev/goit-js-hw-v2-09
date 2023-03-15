b321beaf35cfb7c60b0af80a13d5523bd8791f2d
denis-tsibaev <denis.tsibaev@gmail.com>
2023-03-15 23:03:29 +0700

    Update 01-color-switcher.js
    исправил опечатку

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -18,7 +18,7 @@
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
 
-refs = {
+const refs = {
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
